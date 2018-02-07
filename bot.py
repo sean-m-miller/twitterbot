56cdbab35c6b7d2386193a0cc4ec08e73ee097af
Sean Miller <millersm@whitman.edu>
2018-02-06 17:16:09 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,20 @@
 import random
+import tweepy
+
+# This is setting up our authentication so Twitter knows who we are!
+TWITTER_CONSUMER_KEY = 'E4c30HUmpB8O0fsjA0IVcsP8F'
+TWITTER_CONSUMER_SECRET = 'FBIFejFzogxSdCDBdDriYFpOOoLvYrOPuVSdIvyg286i0VhiRL'
+TWITTER_ACCESS_KEY = '910235671108784129-2fpwQz8UVZkY2aEz3PzLmaaWUhNwQh1'
+TWITTER_ACCESS_SECRET = 'vcpWHzqJtWHiLlrm7NLN69EwugEf2knAUu4Kf7ulTEC7n'
+
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
+
+# Then we initialize our instance of Twitter
+twitter = tweepy.API(auth)
+
+# Now we can send tweets!
+twitter.update_status("Hello World!")
 
 file = open("quotes.txt", "r")
 i = file.read(1)
@@ -7,7 +23,6 @@ numlines = 0
 x = random.randint(1, 3099)
 z = 0
 print(numlines)
-print("wtf")
 print(x)
 #line = file[0]
 for line in file:
@@ -17,5 +32,5 @@ for line in file:
 			while line[i] != "%":
 				newstring+=line[i]
 			if len(newstring) == 0:
-				print("haha loser")
+				print("equals 0")
 
